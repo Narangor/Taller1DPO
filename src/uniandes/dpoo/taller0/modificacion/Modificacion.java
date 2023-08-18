3e72507d0142e12cb3dc20c4c8b82290553f27d1
Nicolás Arango <123480637+Narangor@users.noreply.github.com>
2023-08-18 10:07:33 -0500

    prueba

--- src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -7,10 +7,9 @@ import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
 import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
 
 public class Modificacion {
-	  public static void main(String[] args) throws FileNotFoundException, IOException {
-	    System.out.println("Hola, mundo!");
-	    CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv"); 
-	    System.out.println(calc.paisConMasMedallistas());
-	  }
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		System.out.println("Hola, mundos!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
 	}
-	
+}
